20351164e3cf7e69526105b70d9dddb4669b6468
mahmoud-alwakeel <mahmoudalwakeel200@gmail.com>
2025-02-04 18:32:16 +0200

    routing

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <Link href = "/">Go to Home</Link>
+
+        </div>
+    );
+}
+
+export default About;
--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,20 @@ import Description from "@/components/description";
 import Header from "@/components/header";
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 function Home() {
 
   let showDescription = true;
   return (
-    <main className={styles.section}>
-      <Header text ="Hello from Header"/>
-      {/* we can use any of these 2 for the ternary operator */}
-      {/* {showDescription && <Description />} */}
-      {showDescription ? <Description /> : null}
+    <main >
+      <div className={styles.section}>
+        <Header text="Hello from Header" />
+        {/* we can use any of these 2 for the ternary operator */}
+        {/* {showDescription && <Description />} */}
+        {showDescription ? <Description /> : null}
+      </div>
+      <Link href = "/about">Go to About</Link>
     </main>
   );
 }
