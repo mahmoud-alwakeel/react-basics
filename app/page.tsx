b0d321242d448fa6e8dccd36506046cc7ccd9894
mahmoud-alwakeel <mahmoudalwakeel200@gmail.com>
2025-02-04 17:28:19 +0200

    conditional components

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,14 @@ import Header from "@/components/header";
 import Image from "next/image";
 
 function Home() {
+
+  let showDescription = false;
   return (
     <main>
       <Header text ="Hello from Header"/>
-      <Description />
+      {/* we can use any of these 2 for the ternary operator */}
+      {/* {showDescription && <Description />} */}
+      {showDescription ? <Description /> : null}
     </main>
   );
 }
